16030fb57acaa43bb1f4a51c59a2ae513c214958
ianaverno <vadim.entin@gmail.com>
2015-08-06 17:00:06 +0400

    Add blue curves and subsection numbers

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    images = ["curve1.png", "curve2.png", "curve3.png", "curve4.png", "curve5.png"]
+    number_of_subsections = 15
+    image_repeat = (number_of_subsections * 1.0 / images.length).ceil
+    @images = images.shuffle * image_repeat
   end
 end
