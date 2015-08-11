f6a1d81be2ca4e1ebfcc68e98a82352ffafffbc3
ianaverno <vadim.entin@gmail.com>
2015-08-12 00:45:26 +0400

    English version

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,17 @@
 class WelcomeController < ApplicationController
-  def index
-    images = ["curve1.png", "curve2.png", "curve3.png", "curve4.png", "curve5.png"]
-    number_of_subsections = 15
-    image_repeat = (number_of_subsections * 1.0 / images.length).ceil
-    @images = images.shuffle * image_repeat
+  before_filter :get_images
+
+  def ru
   end
+
+  def en
+  end
+
+  private
+    def get_images
+      images = ["curve1.png", "curve2.png", "curve3.png", "curve4.png", "curve5.png"]
+      number_of_subsections = 15
+      image_repeat = (number_of_subsections * 1.0 / images.length).ceil
+      @images = images.shuffle * image_repeat
+    end
 end
